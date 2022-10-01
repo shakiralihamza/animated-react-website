4d20c77e6a7ac15bf38d556ae1df8585d2c93ae6
Shakir Ali <shakiralihamza05@gmail.com>
2022-10-01 17:09:24 +0500

    Header responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import {Box, Button, Container, Grid, Stack, Typography} from "@mui/material";
+import React, {useState} from 'react';
+import {
+    Box,
+    Button,
+    Container,
+    Drawer,
+    Grid,
+    IconButton,
+    Stack,
+    Typography,
+    useMediaQuery,
+    useTheme
+} from "@mui/material";
 import BarChartIcon from '@mui/icons-material/BarChart';
+import MenuIcon from "@mui/icons-material/Menu";
 
 const menuItems = [
     'Home',
@@ -11,34 +23,82 @@ const menuItems = [
     'Contact Us'
 ];
 
-const Header = () => (
-<Box sx={{position:'fixed', top:0, width:'100%', bgcolor:'rgba(255,255,255,0.98)', zIndex: 999}}>
-    <Container maxWidth={'lg'} sx={{height: '50px' }}>
-        <Grid container justifyContent={'space-between'} alignItems={'center'} sx={{height: '100%'}}>
-            <Grid item xs='auto'>
-                <Stack direction={'row'} alignItems={'center'}>
-                    <BarChartIcon color={'primary'} fontSize={'large'}/>
-                    <Typography variant={'h5'} sx={{fontWeight: '200'}}>touchstream</Typography>
-                </Stack>
-            </Grid>
-            <Grid item xs={6}>
-                <Stack direction={'row'} alignItems={'center'} justifyContent={'center'} spacing={2}>
-                    {
-                        menuItems.map((item, index) => (
-                            <NavButton name={item} key={index}/>
-                        ))
-                    }
-                </Stack>
-            </Grid>
-            <Grid item xs='auto'>
-                <Stack direction={'row'}>
-                    <Button variant={'outlined'} sx={{textTransform: 'none'}}>Request Demo</Button>
-                </Stack>
-            </Grid>
-        </Grid>
-    </Container>
-</Box>
-);
+const Header = () => {
+    const theme = useTheme();
+    const isSmallerThanMD = useMediaQuery(theme.breakpoints.down('md'));
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuOpen = () => {
+        setMenuOpen(true);
+    };
+    const handleMenuClose = () => {
+        setMenuOpen(false);
+    };
+
+    return (
+        <Box sx={{position: 'fixed', top: 0, width: '100%', bgcolor: 'rgba(255,255,255,0.98)', zIndex: 999}}>
+            <Container maxWidth={'lg'} sx={{height: '50px'}}>
+                <Grid container justifyContent={'space-between'} alignItems={'center'} sx={{height: '100%'}}>
+                    <Grid item xs='auto'>
+                        <Stack direction={'row'} alignItems={'center'}>
+                            <BarChartIcon color={'primary'} fontSize={'large'}/>
+                            <Typography variant={'h5'} sx={{fontWeight: '200'}}>touchstream</Typography>
+                        </Stack>
+                    </Grid>
+                    <Grid item xs={6}>
+                        {isSmallerThanMD ?
+                            (<Drawer
+                                anchor={"left"}
+                                open={menuOpen}
+                                onClose={handleMenuClose}
+                                ModalProps={{
+                                    keepMounted: true,
+                                }}
+                            >
+                                <Box sx={{padding: '40px 40px'}}>
+                                    <Stack direction={'column'} alignItems={'flex-start'} justifyContent={'center'}
+                                           spacing={2}>
+                                        {
+                                            menuItems.map((item, index) => (
+                                                <NavButton name={item} key={index}/>
+                                            ))
+                                        }
+                                    </Stack>
+                                </Box>
+                            </Drawer>)
+                            :
+                            <Stack direction={'row'} alignItems={'center'} justifyContent={'center'} spacing={2}>
+                                {
+                                    menuItems.map((item, index) => (
+                                        <NavButton name={item} key={index}/>
+                                    ))
+                                }
+                            </Stack>
+                        }
+                    </Grid>
+                    <Grid item xs='auto'>
+                        <Stack direction={'row'}>
+                            {isSmallerThanMD ?
+
+                                <IconButton
+                                    color="inherit"
+                                    aria-label="open drawer"
+                                    onClick={handleMenuOpen}
+                                    edge="start"
+                                    sx={{mr: 2, ...(!isSmallerThanMD && {display: 'none'})}}
+                                >
+                                    <MenuIcon/>
+                                </IconButton>
+                                :
+                                <Button variant={'outlined'} sx={{textTransform: 'none'}}>Request Demo</Button>
+                            }
+                        </Stack>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
+    );
+}
 
 const NavButton = ({name}) => (
     <Button variant={'text'} color={'inherit'} sx={{fontSize: '0.8rem', textTransform: 'none'}}>{name}</Button>
